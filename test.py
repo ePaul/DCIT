177e2c4e4ead7449b8c78fc457a6bb87db502dbd
kaiserklayton <claytonvioland@gmail.com>
2015-06-06 18:34:49 +0200

    modularized functions

--- test.py
+++ test.py
@@ -1,43 +1,25 @@
 #!/usr/bin/env python
 
+# test.py
 # Authors: C. Clayton Violand & Jessica E. Grasso
 
 from get_dcons import dcon_scrape
 from get_tweets import tweet_scrape
+from get_matches_singles import get_matches_singles
+from get_matches_phrasals import get_matches_phrasals
 
 def main():
-	# Enter custom html and xml file_paths as function arguments below:
+	# Get list of Tweet objects.
 	tweets = tweet_scrape()
+
+	# Get list of Discourse Connective objects.
 	dcons = dcon_scrape()
 
-	# Check for occurances of discourse connectives (type=="single") in tweets.
-	matches = []
-	word_hit_count = 0
-	tweet_hit_count = 0
-	tweet_trigger = False
-	for t in tweets:
-		if tweet_trigger == True:
-			tweet_hit_count += 1
-		tweet_trigger = False
-		for d in dcons:
-			if d.type == "single":
-				if d.name in t.words:
-					tweet_trigger = True
-					word_hit_count += 1
-					matches.append((t,d))
+	# Check for occurances of Discourse Connectives (type=="single") in Tweets.
+	matches_singles = get_matches_singles()
 
-	ratio = tweet_hit_count / float(len(tweets))
-
-	print
-	print "--SUMMARY--" 
-	print "-----------------------------------"
-	print "Discourse Connective Type: \"single\""
-	print "--------------------------------------------------------------------"
-	print "Found %d Discourse Connectives amongst %d Tweets." % (word_hit_count, len(tweets))
-	print "Found a Discourse Connective in %d out of %d Tweets." % (tweet_hit_count, len(tweets))
-	print "Tweet Saturation is %f." % ratio
-	print "--------------------------------------------------------------------"
-	print
+	# Check for occurances of Discourse Connectives (type=="phrasal") in Tweets.
+	matches_phrasals = get_matches_phrasals()
 
 if __name__ == "__main__":
 	main()
