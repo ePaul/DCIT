1873b578eda8d32af3bac3532bb944777f08bc81
jgrasso <jgrasso@uni-potsdam.de>
2015-08-19 11:27:32 +0200

    write_results updated

--- DCIT_Tool/write_results.py
+++ DCIT_Tool/write_results.py
@@ -19,6 +19,7 @@ def write_results(tweets, input_path, output_path):
 			outfile.close()
 	
 	for t in tweets:
+			
 		if t.filename != currentfile:
 			# every time we see a new file, write old one and open new one
 			write()	
@@ -29,11 +30,19 @@ def write_results(tweets, input_path, output_path):
 		results = soup.findAll("tweet", {"id" : t.id})
 		# there should always be one and only one match, hence results[0]
 		# add some additional attributes (just an example)
-		results[0]["has_discourse_connective"] = t.has_dc()	
-		
-		
+		results[0]["has_dc"] = t.has_dc()	
+		results[0]["num_dcs"] = len(t.dcs)
 		
 		# !!! should also tag the specific word?
+		for d in t.dcs:
+			
+			results[0]["dc_location"] = d[2]
+			
+			# or include name of DC?
+			#results[0]["dc_location_"+d[0].part_one[0]] = d[2]
+			
+			# CHECK THIS for multiple DCs in one tweet, and
+			# for discontinuous DCs, none in current test file!
 		
 	write() # write the last one
 
